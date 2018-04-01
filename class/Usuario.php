bf2ccd68a9424f7b0794f831335baf077c0fa1f5
thiago leite <thiagoleite.ep@gmail.com>
2018-04-01 11:53:11 +0100

    Add insert and update methods to the Usuario class and some auxiliar methods to manipulate the object

--- class/Usuario.php
+++ class/Usuario.php
@@ -2,11 +2,13 @@
 
 class Usuario{
 
+	/* Atributos da classe */
 	private $idusuario;
 	private $deslogin;
 	private $dessenha;
 	private $dtcadastro;
 
+	// ----- Métodos de acesso aos atributos da classe ----- //
 	public function getIdusuario(){
 		return $this->idusuario;
 	}
@@ -39,33 +41,14 @@ class Usuario{
 		$this->dtcadastro = $value;
 	}
 
-	public static function selectAll():array{
-		$sql = new Sql();
-		return $sql->select("SELECT * FROM tb_usuarios ORDER BY dtcadastro");
+	// ----- Métodos internos de manipulação da classe ----- //
+	/* Método construtor */
+	public function __construct($deslogin = "", $dessenha = ""){
+		$this->setDeslogin($deslogin);
+		$this->setDessenha($dessenha);
 	}
 
-	public static function selectByLogin($login):array{
-		$sql = new Sql();
-		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :search ORDER BY dtcadastro", array(
-				':search'=>"%".$login."%"
-			)
-		);
-	}
-
-	public function selectById($id){
-		$sql = new Sql();
-		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
-
-		if(count($results) > 0){
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
-		}
-	}
-
+	/* Métodos sobrecarregados/ herdados */
 	public function __toString(){
 		return json_encode(array(
 				"idusuario"=>$this->getIdusuario(),
@@ -74,6 +57,81 @@ class Usuario{
 				"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 			));
 	}
+
+	/* Métodos próprios */
+	public function setData($data){
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+	// ----- Métodos de acesso ao banco ----- //
+	/* Selecionar todos os usuários*/
+	public static function selectAll():array{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY dtcadastro");
+	}
+
+	/* Selecionar lista de usuários baseado no login */
+	public static function selectByLogin($login):array{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :search ORDER BY dtcadastro", array(
+				':search'=>"%".$login."%"
+			)
+		);
+	}
+
+	/* Selecionar um usuário por ID*/
+	public function selectById($id){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
+
+		if(count($results) > 0) $this->setData($results[0]);
+	}
+
+	/* Inserir um usuário */
+	public function insert(){
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+				':LOGIN'=>$this->getDeslogin(),
+				':PASSWORD'=>$this->getDessenha()
+			));
+
+		if(count($results) > 0) $this->setData($results[0]);
+	}
+
+	/* Atualizar um usuário*/
+	public function update($deslogin, $dessenha){
+		
+		$this->setDeslogin($deslogin);
+		$this->setDessenha($dessenha);
+
+		$sql = new Sql();
+
+		$results = $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+				':LOGIN'=>$this->getDeslogin(),
+				':PASSWORD'=>$this->getDessenha(),
+				':ID'=>$this->getIdusuario()
+			));
+	}
+
+	// ----- Métodos de ação - negócio ----- //
+	/* Logar um usuário */
+	public function login($login, $password){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN and dessenha = :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password));
+
+		if(count($results) > 0) $this->setData($results[0]);
+		else{
+			throw new Exception("Login e/ ou senha inválidos.",1);
+		}
+	}
+
+	
+
+
 
 }
 
--- index.php
+++ index.php
@@ -11,8 +11,22 @@ echo $root;*/
 //echo json_encode(Usuario::selectAll());
 
 // select as many as the login are similar
-echo json_encode(Usuario::selectByLogin("a"));
+//echo json_encode(Usuario::selectByLogin("a"));
 
+// try to login Usuario
+//$usuario = new Usuario();
+//$usuario->login("pedro", "ambndisdk");
+//echo $usuario;
 
+// insert usuario
+//$aluno = new Usuario("aluno", "sa$%g4g");
+//$aluno->insert();
+//echo $aluno;
+
+// update usuario
+$usuario = new Usuario();
+$usuario->selectById(8);
+$usuario->update("Thiallisson","dskfs934fm34343c343");
+echo $usuario;
 
 ?>
